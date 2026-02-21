cd37b9252c7cd068f25a2db7d64f5d942bf132c0
adonislive <156614476+adonislive@users.noreply.github.com>
2026-02-21 13:21:46 -0800

    Distinguish between localhost vs LAN setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,10 @@ echo "Starting EQMacEmu..."
 service mariadb start
 sleep 3
 
+# Patch server address from environment variable into config
+# Default is 127.0.0.1 (local only). Set SERVER_ADDRESS in docker-compose.yml for LAN/internet.
+sed -i "s/\"address\": \"127.0.0.1\"/\"address\": \"${SERVER_ADDRESS}\"/g" /src/build/bin/eqemu_config.json
+
 cd /src/build/bin
 
 # Start shared_memory
