ada6ebfda4a7684354f6879bb3a3eb63a860d8a1
adonislive <156614476+adonislive@users.noreply.github.com>
2026-02-21 19:42:54 -0800

    Fixed LAN connectivity: patch server addresses at startup
    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,9 @@ sleep 3
 # Patch server address from environment variable into config
 # Default is 127.0.0.1 (local only). Set SERVER_ADDRESS in docker-compose.yml for LAN/internet.
 sed -i "s/\"address\": \"127.0.0.1\"/\"address\": \"${SERVER_ADDRESS}\"/g" /src/build/bin/eqemu_config.json
-sed -i "s/\${SERVER_ADDRESS}/${SERVER_ADDRESS}/g" /src/build/bin/login.json
+sed -i "s/\"localaddress\": \"127.0.0.1\"/\"localaddress\": \"${SERVER_ADDRESS}\"/g" /src/build/bin/eqemu_config.json
+sed -i "s/\"local_network\": \"127.0.0.1\"/\"local_network\": \"${SERVER_ADDRESS}\"/g" /src/build/bin/login.json
+sed -i "s/\"network_ip\": \"127.0.0.1\"/\"network_ip\": \"${SERVER_ADDRESS}\"/g" /src/build/bin/login.json
 
 cd /src/build/bin
 
